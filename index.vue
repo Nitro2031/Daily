061170d75da20a9cce6498671e9031764117afae
NITOH Hisashi <support@amus.biz>
2026-03-04 18:07:43 +0900

    ヘッダーのメニュー内に「バックアップをリストア」を追加

---
+++ index.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-app-bar app>
+        <v-app-bar-nav-icon @click="menu = !menu" />
+
+        <v-menu v-model="menu" activator="parent">
+            <v-list>
+                <v-list-item @click="backupData">
+                    <v-list-item-title>バックアップを保存</v-list-item-title>
+                </v-list-item>
+
+                <v-list-item @click="restoreBackup">
+                    <v-list-item-title>バックアップをリストア</v-list-item-title>
+                </v-list-item>
+
+                <v-list-item @click="openSettings">
+                    <v-list-item-title>設定</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            menu: false
+        }
+    },
+    methods: {
+        backupData() {
+            // IndexedDB → JSON エクスポート
+        },
+        restoreBackup() {
+            // JSON → IndexedDB インポート
+            // ファイル選択ダイアログを開く
+        },
+        openSettings() {
+            // 設定画面を開く
+        }
+    }
+}
+</script>
